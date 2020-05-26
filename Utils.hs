c7471de491fb2840421f9a09e6b6f4a011a88cc6
Zane Othman-Gomez <1nk5t0n3@gmail.com>
2020-05-25 21:23:52 -0700

    Implemented a more general splitEveryChar function

--- Utils.hs
+++ Utils.hs
@@ -1,22 +1,27 @@
 module Utils
-  ( commaIndex
-  , splitAtCommas
+  ( splitAtCommas
   , replaceNth
+  , splitAtChar
+  , splitEveryChar
   ) where
 
 import Data.List
 
-commaIndex :: String -> Maybe Int
-commaIndex = elemIndex ','
-
 splitAtCommas :: String -> [String]
-splitAtCommas "" = []
-splitAtCommas rawString = case commaIndex rawString of
-                            Just idx ->
-                              -- Omit comma
-                              let (headInput,_:restInput) = splitAt idx rawString
-                              in headInput : splitAtCommas restInput
-                            Nothing -> return rawString
+splitAtCommas = splitEveryChar ','
+
+splitAtChar :: Char -> String -> (String,String)
+splitAtChar _ "" = ("","")
+splitAtChar c str = (before,after)
+  -- Discard character in question
+  where (before,_:after) = case elemIndex c str of
+                             Just _ -> break (==c) str
+                             Nothing -> (str," ")
+
+splitEveryChar :: Char -> String -> [String]
+splitEveryChar _ "" = []
+splitEveryChar c str = first : splitEveryChar c rest
+  where (first,rest) = splitAtChar c str
 
 replaceNth :: Int -> a -> [a] -> [a]
 replaceNth _ _ [] = []
