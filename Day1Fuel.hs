2057f5535a7dc3c208cdec36c01269a1fef7400f
Zane Othman <zane.othman@gmail.com>
2020-12-22 16:20:56 -0800

    Start of changes to be more idiomatic
    Replaced foldl1 (+) with sum in Day1Fuel.hs.
    Formatting should also be more consistent without random whitespace.

--- Day1Fuel.hs
+++ Day1Fuel.hs
@@ -4,17 +4,16 @@ part1 :: IO Int
 part1 = do
   input <- readFile "input/day1input.txt"
   let fuelAmountList = map read . lines $ input
-      fuelMass = foldl1 (+) . map moduleFuelNaive $ fuelAmountList
+      fuelMass = sum . map moduleFuelNaive $ fuelAmountList
   return fuelMass
-  
 
 part2 :: IO Int
 part2 = do
   input <- readFile "input/day1input.txt"
   let fuelAmountList = map read . lines $ input
-      fuelMass = foldl1 (+) . map moduleFuel $ fuelAmountList
+      fuelMass = sum . map moduleFuel $ fuelAmountList
   return fuelMass
-  
+
 moduleFuelNaive :: Int -> Int
 moduleFuelNaive mass = div mass 3 - 2
 
@@ -22,4 +21,5 @@ moduleFuel :: Int -> Int
 moduleFuel mass
   | mass > 0 && newMass > 0 = newMass + moduleFuel newMass
   | otherwise = 0
-  where newMass = div mass 3 - 2
+  where
+    newMass = div mass 3 - 2
