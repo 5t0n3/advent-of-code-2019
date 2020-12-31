f1f005f25e3abfbe17bf84bb3265734ad80bb165
Zane Othman <zane.othman@gmail.com>
2020-12-31 14:38:50 -0800

    Added day 8 part 2 solution

--- Day08Image.hs
+++ Day08Image.hs
@@ -5,6 +5,8 @@ import Data.List (elemIndices)
 
 type Layer = [Int]
 
+type Image = [Layer]
+
 type Dimensions = (Int, Int)
 
 imageDimensions :: Dimensions
@@ -13,10 +15,10 @@ imageDimensions = (25, 6) -- Columns, rows
 totalPixels :: Int
 totalPixels = uncurry (*) imageDimensions
 
-parseLayers :: String -> [Layer]
-parseLayers [] = []
-parseLayers raw =
-  map digitToInt fullLayer : parseLayers otherLayers
+parseImage :: String -> Image
+parseImage [] = []
+parseImage raw =
+  map digitToInt fullLayer : parseImage otherLayers
   where
     (cols, rows) = imageDimensions
     (fullLayer, otherLayers) = splitAt totalPixels raw
@@ -24,9 +26,35 @@ parseLayers raw =
 part1 :: IO Int
 part1 = do
   rawInput <- readFile "input/day08.txt"
-  let parsedInput = parseLayers rawInput
+  let parsedInput = parseImage rawInput
       leastZeroesLayer = foldl1 (\acc layer -> if zeroCount layer < zeroCount acc then layer else acc) parsedInput
   return $ elemCount 1 leastZeroesLayer * elemCount 2 leastZeroesLayer
   where
     elemCount elem = length . elemIndices elem
     zeroCount = elemCount 0
+
+part2 :: IO Layer
+part2 = do
+  rawInput <- readFile "input/day08.txt"
+  let parsedInput = parseImage rawInput
+      shownImage = map (topOpaquePixel parsedInput) [0 .. totalPixels - 1]
+  putStrLn $ formatImage shownImage
+  return shownImage
+
+topOpaquePixel :: Image -> Int -> Int
+topOpaquePixel [] _ = 2
+topOpaquePixel (top : rest) idx
+  | topPixel == 2 = topOpaquePixel rest idx
+  | otherwise = topPixel
+  where
+    topPixel = top !! idx
+
+formatImage :: Layer -> String
+formatImage [] = []
+formatImage img = map displayDigit current ++ '\n' : formatImage rest
+  where
+    (cols, rows) = imageDimensions
+    (current, rest) = splitAt cols img
+    displayDigit 0 = '-'
+    displayDigit 1 = 'O'
+    displayDigit 2 = ' '
