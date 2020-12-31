3c3e1cff78a297686f3567d43ea2d82b84de49f8
Zane Othman <zane.othman@gmail.com>
2020-12-31 14:48:23 -0800

    Partially rewrote day 2 solution

--- Day08Image.hs
+++ Day08Image.hs
@@ -1,5 +1,6 @@
 module Day08Image where
 
+import Control.Monad (join)
 import Data.Char (digitToInt)
 import Data.List (elemIndices)
 
@@ -37,23 +38,23 @@ part2 :: IO Layer
 part2 = do
   rawInput <- readFile "input/day08.txt"
   let parsedInput = parseImage rawInput
-      shownImage = map (topOpaquePixel parsedInput) [0 .. totalPixels - 1]
+      shownImage = map topOpaquePixel $ groupOverlappingPixels parsedInput
   putStrLn $ formatImage shownImage
   return shownImage
 
-topOpaquePixel :: Image -> Int -> Int
-topOpaquePixel [] _ = 2
-topOpaquePixel (top : rest) idx
-  | topPixel == 2 = topOpaquePixel rest idx
-  | otherwise = topPixel
-  where
-    topPixel = top !! idx
+groupOverlappingPixels :: Image -> [[Int]]
+groupOverlappingPixels img
+  | null $ join img = []
+  | otherwise = map head img : groupOverlappingPixels (map tail img)
+
+topOpaquePixel :: [Int] -> Int
+topOpaquePixel = head . dropWhile (== 2)
 
 formatImage :: Layer -> String
 formatImage [] = []
 formatImage img = map displayDigit current ++ '\n' : formatImage rest
   where
-    (cols, rows) = imageDimensions
+    cols = fst imageDimensions
     (current, rest) = splitAt cols img
     displayDigit 0 = '-'
     displayDigit 1 = 'O'
